abf894ffab1e81dbc893f286430b1423a948010f
mbeebepdx <mbeebe08@gmail.com>
2020-10-16 10:22:26 -0700

    Initial commit, initial solution

---
+++ src/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var numBottles = 99
+
+    while (numBottles > 0) {
+        println("$numBottles bottles of beer on the wall")
+        println("$numBottles bottles of beer")
+        println("Take one down, pass it around")
+        numBottles--
+        println("$numBottles bottles of beer on the wall \n")
+
+
+        println("$numBottles bottles of beer on the wall")
+        println("$numBottles bottles of beer")
+        println("Take one down, pass it around")
+        numBottles--
+        println("$numBottles bottles of beer on the wall \n")
+    }
+
+}
