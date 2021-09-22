b4deb409914c2e66032a48bbbfb3998c1d1dbcec
Mark Beebe <mbeebe08@gmail.com>
2021-09-22 14:35:06 -0700

    feat: add interface to main

--- src/main/kotlin/Bottle.kt
+++ src/main/kotlin/Bottle.kt
@@ -1,1 +1,23 @@
-class Bottle
+private interface BottleInterface {
+
+    fun verse(bottleNumber: Int): String
+
+    fun verses(startingBottleNumber: Int, endingBottleNumber: Int): String
+
+    fun song(): String
+}
+
+class Bottle : BottleInterface {
+    override fun verse(bottleNumber: Int): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun verses(startingBottleNumber: Int, endingBottleNumber: Int): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun song(): String {
+        TODO("Not yet implemented")
+    }
+
+}
